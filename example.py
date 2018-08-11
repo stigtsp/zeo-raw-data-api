e786c8aa11f5230aa132ae9a5b84cb486b53897f
Hypnomachia <Hypnomachia@somicas-mbp.home>
2018-08-11 16:37:21 +0200

    updated for osc

--- example.py
+++ example.py
@@ -2,26 +2,46 @@
 from ZeoRawData.BaseLink import BaseLink
 from ZeoRawData.Parser import Parser
 
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+import json
+
 # Initialize
 link = BaseLink("/dev/tty.usbserial-FTHBZZ4L")
 parser = Parser()
 
 # Add the Parser to the BaseLink's callback
 link.addCallback(parser.update)
-
+client = udp_client.SimpleUDPClient("127.0.0.1", 13000)
+client2 = udp_client.SimpleUDPClient("127.0.0.1", 5005)
 # Add your callback functions
 
-def linkCallbackDebug(timestamp, timestamp_subsec, version, data):
-    print("timestamp {0}, subsec {1}, version {2}, data {3}\n".format(timestamp, timestamp_subsec, version, data))
+#def linkCallbackDebug(timestamp, timestamp_subsec, version, data):
+#    print("timestamp {0}, subsec {1}, version {2}, data {3}\n".format(timestamp, timestamp_subsec, version, data))
 
 #link.addCallback(linkCallbackDebug)
 
-def foo(a):
-    print(a)
+def sendToOSC(s):
+    if not s:
+        return False
+    
+    for k in ['ZeoTimestamp',  'Impedance', 'SQI', 'Version', 'Waveform', 'SleepStage']:
+        if s[k]:
+            print(s[k])
+            client.send_message("/" + k, s[k])
+            client2.send_message("/" + k, s[k])
+
+        if s['FrequencyBins']:
+            client.send_message("/FrequencyBins", s['FrequencyBins'].values())
+
+        client.send_message("/BadSignal", s.get('BadSignal', False))
+
+            
 
 
-parser.addEventCallback(foo)
-parser.addSliceCallback(foo)
+#parser.addEventCallback(sendToOSC)
+parser.addSliceCallback(sendToOSC)
 
 # Start link
 link.start()
